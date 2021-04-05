0a137c5c2bce00b54072cda29f04b3b3bf40ba36
avigunjal <avinasshgunjal@gmail.com>
2021-04-05 11:36:40 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hooks advance concepts..</h1>
+        <h1>Hooks advance concepts...</h1>
       </header>
     </div>
   );
